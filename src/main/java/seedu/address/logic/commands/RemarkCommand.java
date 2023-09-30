e8157f7652c2470c613793550d0c369384333c86
GohTengFong <tengfonggoh@gmail.com>
2023-09-30 15:00:27 +0800

    Fix styles

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -17,6 +17,9 @@ import seedu.address.model.person.Remark;
  * Changes the remark of an existing person in the address book.
  */
 public class RemarkCommand extends Command {
+    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
+
+    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
 
     public static final String COMMAND_WORD = "remark";
 
@@ -33,10 +36,6 @@ public class RemarkCommand extends Command {
     private final Index index;
 
     private final Remark remark;
-
-    public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
-
-    public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
 
     /**
      * @param index of the person in the filtered person list to edit the remark
